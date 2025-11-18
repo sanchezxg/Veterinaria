a7ee3c51e3af22de3204dd94974bb5a88a33078a
Diego-Alejo <bstivenacetobon888@gmail.com>
2025-11-18 08:59:31 -0500

    1

---
+++ modelo/Entidades/medico.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace modelo.Entidades
+{
+    public class medico
+    {
+        public int Id { get; set; } //Campo obligatorio, es la clave primaria por defecto
+
+        public string Nombre { get; set; } = string.Empty;
+
+        public string Descripcion { get; set; } = string.Empty;
+
+        public int Codigo { get; set; }
+
+
+    }
+}
---
+++ veterinariaback/Data/DataContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+using modelo.Entidades;
+
+
+namespace veterinariaback.Data
+{
+    public class DataContext:DbContext
+    {
+
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+
+        {
+
+        }
+
+
+        public DbSet<medico> Productos { get; set; }
+
+    }
+}
+
